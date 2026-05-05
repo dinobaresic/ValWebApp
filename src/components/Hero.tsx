8e9f734e4478f0bb360d8382f9f5d520da7446ba
dinobaresic <dinobaresic@unizd.hr>
2026-05-06 01:42:34 +0200

    perf: add poster image to hero video for better perceived performance

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -32,6 +32,7 @@ export default function Hero() {
             loop
             muted
             playsInline
+            poster="/images/photo-1.jpg"
             className="absolute top-1/2 left-1/2 w-[100vw] h-[100vh] -translate-x-1/2 -translate-y-1/2 object-cover opacity-30 mix-blend-screen"
             style={{ pointerEvents: 'none' }}
           >
